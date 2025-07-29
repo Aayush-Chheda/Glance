9aa151317a8efe7e150352500d76bd560a7a8ee6
Mayank Chopra <mayankchopra144@gmail.com>
2025-07-29 23:43:19 +0530

    mdc : adding email constructor utility

---
+++ backend/src/main/java/com/glance/backend/utility/EmailConstructor.java
@@ -0,0 +1,43 @@
+package com.glance.backend.utility;
+
+import com.glance.backend.model.AppUser;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import org.hibernate.cfg.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+public class EmailConstructor {
+
+    @Autowired
+    private Environment env;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    public MimeMessagePreparator constructNewUserEmail(AppUser appUser,String password){
+        Context context = new Context();
+        context.setVariable("user", appUser);
+        context.setVariable("password",password);
+
+        String text = templateEngine.process("newUserEmailTemplate",context);
+
+        MimeMessagePreparator messagePrepartor = new MimeMessagePreparator() {
+            @Override
+            public void prepare (MimeMessage mimeMessage) throws Exception{
+                MimeMessageHelper email = new MimeMessageHelper(mimeMessage);
+                email.setPriority(1);
+                email.setTo(appUser.getEmail());
+                email.setSubject("Welcome To Glance");
+                email.setText(text, true);
+                email.setFrom(new InternetAddress(env.getProperties().getProperty("spring.mail.username")));
+            }
+        };
+
+        return messagePrepartor;
+    }
+
+}
